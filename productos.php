21e733785e3a6beb1a645f3e32d0729933be248b
Tobias Echenique <tobiasechenique>
2022-06-05 18:02:16 -0300

    paginacion y seccion de productos CASI terminada

--- productos.php
+++ productos.php
@@ -17,6 +17,11 @@
     <?php
         include("./webTools/nav.php");
 
+        if(!$_GET){
+            header('location:productos.php?pagina=1');
+        }
+
+
         $sql = "SELECT * FROM productos ORDER BY id desc"; 
         $resultado = $db->query($sql);
         
@@ -32,27 +37,37 @@
         }
 
         $resultados_por_pagina = 8;
+        $id = $resultados_por_pagina * ($_GET['pagina'] - 1);
 
         $query = mysqli_query($db,"SELECT id FROM productos");
         $total_resultados = mysqli_num_rows($query);
 
         $numero_paginas = ceil ($total_resultados / $resultados_por_pagina);
 
+
+
     ?>    
         <div class="container-xl" style="padding-top: 50px">
             <div class="row">
     
     <?php
-        for($i = 0; $i < $total_resultados; $i++){
-                $query = mysqli_query($db,"SELECT * FROM productos WHERE id = '$idarray[$i]'");
-                $res = mysqli_fetch_array($query);
+        for($i = 0; $i < $resultados_por_pagina; $i++, $id++){
 
-                $nombre = $res['nombre'];
-                $precio = '$' . $res['precio'];
-                $img = 'uploaded-img/' . $res['img'];
+            if ($id >= $total_resultados) {
+                break;
+            }
+
+            $query = mysqli_query($db,"SELECT * FROM productos WHERE id = '$idarray[$id]'");
+            $res = mysqli_fetch_array($query);
+
+
+            $nombre = $res['nombre'];
+            $precio = '$' . $res['precio'];
+            $img = 'uploaded-img/' . $res['img'];
+        
     ?>
             
-            <div class="col-md-3 col-sm-6 mx-auto">
+            <div class="col-md-3 col-sm-6 mx-auto" style="padding-top: 15px;">
                 <div class="card rounded" style="width: 105%; text-align: center;">
 
                     <img class="card-img-top" src= "<?php echo $img; ?>" alt="product_img"> <!-- imagen -->
@@ -71,7 +86,25 @@
             </div>
         </div>
     
-    
+    <div class="container-sm" style="padding-top: 15px;">
+        <nav aria-label="paginacion">
+            <ul class= "pagination">
+                <li class="page-item 
+                    <?php echo $_GET['pagina'] <= 1? 'disabled':'' ?>
+                "><a class="page-link" href="productos.php?pagina=<?php echo $_GET['pagina']-1;?>">Anterior</a></li>
+
+                <?php for($i = 0; $i < $numero_paginas; $i++){ ?>
+                    <li class="page-item <?php echo $_GET['pagina']==$i+1? 'active': '' ?>"><a class="page-link" href="productos.php?pagina=<?php echo $i+1 ?>"><?php echo $i+1; ?></a></li>
+                <?php } ?>
+
+                <li class="page-item
+                    <?php echo $_GET['pagina'] >= $numero_paginas? 'disabled':'' ?>
+                "><a class="page-link" href="productos.php?pagina=<?php echo $_GET['pagina']+1;?>">Siguiente</a></li>
+            </ul>
+
+        </nav>
+    </div>
+
     <!-- CODIGO ORDENADO
     <div class="container-xl" style="padding-top: 50px">
         <div class="row">
