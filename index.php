7633cc0443eea7af0ef3ffa0b669c9dce6f03506
Tobias Echenique <tobiasechenique>
2022-06-06 23:05:10 -0300

    agregado carrusel en index y cambio de fuente

--- index.php
+++ index.php
@@ -33,11 +33,34 @@
     
     <!--PRODUCTO DESTACADO-->
     <header>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="./images/skate-banner-2.gif" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="./images/banner.gif" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="./images/santa-cruz-banner.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <!--
         <div class="container-fluid" style="padding-top: 25px; padding-bottom: 25px; border-bottom: 1px solid black; background-color: black;">
             <div class="row">
                 <img src="./images/banner.gif" alt="prodDestacado" class="rounded">
             </div>
         </div>
+        -->
     </header>
     
     <?php include("./webTools/footer.php"); ?>
